a072daa0fc07eaf362bab035a00cfb80e8f95122
lb <lb.@lb.local>
2017-08-15 09:24:25 +0800

    1.修改error

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,7 +1,8 @@
 
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-	}
+public class HelloWorld {  
+    
+    public static void main(String[] args) {
+        System.out.println("HelloWorld！");
+    }
+    
 }
